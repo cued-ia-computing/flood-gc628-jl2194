462aaa17e66af28a9a3e7634da454c76e67f42ab
George Cooke <gc628@cam.ac.uk>
2021-01-23 13:21:50 +0000

    Task 1B build error fix 3

--- Task1B.py
+++ Task1B.py
@@ -3,23 +3,23 @@ from floodsystem.geo import stations_by_distance
 
 
 def run():
-    #Requirements for Task 1A
+    """ Requirements for Task 1B"""
 
-    # Build list of stations
+    # build list of stations
     stations = build_station_list()
 
-    #sort stations
+    # sort stations
     sorted_stations = stations_by_distance(stations, (52.2053, 0.1218))
 
-    #print data from closest 10
+    # print data from closest 10
     print("Closest 10")
     print([(stat[0].name, stat[0].town, stat[1]) for stat in sorted_stations[:10]])
 
-    #print data from furthest 10
+    # print data from furthest 10
     print("Furthest 10")
     print([(stat[0].name, stat[0].town, stat[1]) for stat in sorted_stations[len(stations) - 10:]])
-   
-    
+
+
 if __name__ == "__main__":
     print("*** Task 1A: CUED Part IA Flood Warning System ***")
     run()
--- test_geo.py
+++ test_geo.py
@@ -1,11 +1,11 @@
-#unit tests for geo module
+"""unit tests for geo module"""
 from floodsystem.geo import stations_by_distance
 from floodsystem.station import MonitoringStation
 import pytest
 
 
 def test_stations_by_distance():
-    #testing for correct order and value, checked by hand
+    # testing for correct order and value, checked by hand
     s_id = "test-s-id"
     m_id = "test-m-id"
     label = "some station"
@@ -18,23 +18,22 @@ def test_stations_by_distance():
 
     sorted_stations = stations_by_distance(stations, (0.0, 0.0))
 
-    #assert correct order
+    # assert correct order
     assert sorted_stations[0][0].town == "1,0"
     assert sorted_stations[1][0].town == "1,1"
     assert sorted_stations[2][0].town == "0,-1.5"
 
-    #assert correct value
+    # assert correct value
     assert round(sorted_stations[0][1], 3) == 111.195
     assert round(sorted_stations[1][1], 3) == 157.250
     assert round(sorted_stations[2][1], 3) == 166.793
 
-    #test validation for point
+    # test validation for point
     with pytest.raises(TypeError) as e:
         stations_by_distance(stations, ("This is an invlaid point"))
     assert "point" in str(e)
 
-    #test validation for station
+    # test validation for station
     with pytest.raises(TypeError) as e:
         stations_by_distance("This is an invlaid station", (0.0, 0.0))
     assert "station" in str(e)
-    
